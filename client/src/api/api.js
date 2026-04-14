2e637b0dddee2b862aa3df3aaa1c1ca7d8adbe10
Khushbu Yadav <your-email@example.com>
2026-04-14 15:26:18 +0530

    api

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 
+console.log("DEBUG: Connecting to Backend at:", import.meta.env.VITE_API_URL || "LOCAL_RELATIVE_PATH");
+
 const API = axios.create({
   baseURL: import.meta.env.VITE_API_URL ? `${import.meta.env.VITE_API_URL}/api` : "/api"
 });
